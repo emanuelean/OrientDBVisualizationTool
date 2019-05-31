bfe017ded950dbd0b5031ca44eb0da9dc4d55ccc
User <n.a.bugel@student.rug.nl>
2019-05-31 12:07:43 +0200

    Tiny refactoring in entityAttributes

--- OrientDBVisualizationTool/src/main/java/com/mycompany/orientdbvisualizationtool/database/EntityAttributes.java
+++ OrientDBVisualizationTool/src/main/java/com/mycompany/orientdbvisualizationtool/database/EntityAttributes.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.mycompany.orientdbvisualizationtool.database;
 
@@ -13,27 +13,38 @@ import com.tinkerpop.blueprints.Vertex;
  * @author Carlos
  *
  */
-public class EntityAttributes{
-	private ArrayList<String> types;
-	
-	public EntityAttributes (Vertex v){
-		Iterable<Edge> parents;
-		types = new ArrayList();
-		parents = v.getEdges(Direction.OUT, "instance-of");
-		for(Edge e : parents) {
-			try {
-				types.add(e.getVertex(Direction.IN).getProperty("type-id"));
-			} catch (Exception ex) {
-				continue;
-			}
-		}
-	}
+public class EntityAttributes {
 
-	/**
-	 * @return the types
-	 */
-	public ArrayList<String> getTypes() {
-		return types;
-	}
+    private ArrayList<String> types;
+
+    public EntityAttributes(Vertex v) {
+        Iterable<Edge> parents;
+        types = new ArrayList();
+        parents = v.getEdges(Direction.OUT, "instance-of");
+        for (Edge e : parents) {
+            if(hasProperty(e)) {
+                types.add(e.getVertex(Direction.IN).getProperty("type-id"));
+            }
+        }
+    }
+
+    private boolean hasProperty(Edge edge) {
+        try {
+            String value = edge.getVertex(Direction.IN).getProperty("type-id");
+            if (value == null) {
+                return false;
+            }
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * @return the types
+     */
+    public ArrayList<String> getTypes() {
+        return types;
+    }
 
 }
